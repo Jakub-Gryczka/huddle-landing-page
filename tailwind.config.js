a54f931010601e4cbc079f5167eb2079dae143a1
Jakub Gryczka <gryczka.jakub@gmail.com>
2023-12-26 20:40:59 +0100

    feat: finished hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    colors: {
-      'pink-primary': 'hsl(322, 100%, 66%)',
-      neutral: {
-        'pale-cyan': 'hsl(193, 100%, 96%)',
-        'dark-cyan': 'hsl(192, 100%, 9%)',
-        'grayish-cyan': 'hsl(208, 11%, 55%)',
-      },
-    },
     extend: {
+      colors: {
+        'pink-primary': 'hsl(322, 100%, 66%)',
+        neutral: {
+          'pale-cyan': 'hsl(193, 100%, 96%)',
+          'dark-cyan': 'hsl(192, 100%, 9%)',
+          'grayish-cyan': 'hsl(208, 11%, 55%)',
+        },
+      },
       fontFamily: {
         Poppins: ['Poppins', 'sans-serif'],
         Open_Sans: ['Open Sans'],
+      },
+      backgroundImage: {
+        'hero-background-mobile': "url('assets/images/bg-hero-mobile.svg')",
+        'hero-background-desktop': "url('assets/images/bg-hero-desktop.svg')",
       },
     },
   },
