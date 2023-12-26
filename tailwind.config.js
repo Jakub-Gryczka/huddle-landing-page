fd1837299654ce27dbe7c7f880d5a3a26e4efd72
Jakub Gryczka <gryczka.jakub@gmail.com>
2023-12-26 18:00:31 +0100

    chore: project setup, added used technologies in project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      'pink-primary': 'hsl(322, 100%, 66%)',
+      neutral: {
+        'pale-cyan': 'hsl(193, 100%, 96%)',
+        'dark-cyan': 'hsl(192, 100%, 9%)',
+        'grayish-cyan': 'hsl(208, 11%, 55%)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+        Open_Sans: ['Open Sans'],
+      },
+    },
+  },
+  plugins: [],
+};
